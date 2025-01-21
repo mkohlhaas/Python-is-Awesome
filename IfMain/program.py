8f9488bcfdd06bf64dc448a359c052e34ed58e21
Hans-J. Schmid <knock@myopendoor.de>
2025-01-21 20:07:58 +0100

    if main

---
+++ IfMain/program.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import test
+
+print(__name__)
+
+if __name__ == "__main__":
+    print(test.hello("program"))
---
+++ IfMain/test.py
@@ -0,0 +1,9 @@
+print(__name__)
+
+
+def hello(name: str):
+    return f"Hello, {name}!"
+
+
+if __name__ == "__main__":
+    print(hello("test"))
