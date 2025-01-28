b3681a680517604a9857594f76603e9906fa084f
Hans-J. Schmid <knock@myopendoor.de>
2025-01-28 18:26:39 +0100

    click

---
+++ click-tut/README.md
@@ -0,0 +1,13 @@
+### Plugins
+
+Under Archlinux you can install following plugins:
+
+```shell
+sudo pacman -S python-click-aliases
+               python-click-command-tree \
+               python-click-completion \
+               python-click-didyoumean \
+               python-click-help-colors \
+               python-click-log \
+               python-click-repl \
+```
---
+++ click-tut/hello.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import click
+
+VERSION = "0.1.0"
+
+
+@click.command()
+@click.version_option(VERSION)
+@click.argument("names", nargs=2)
+@click.option("-a", "--age", type=int, default=0)
+# @click.option("-s", "--shout", is_flag=True)
+@click.option("--shout/--no-shout")
+@click.option("-f", "--file", type=click.File("w"))
+def profile(names, age, shout, file):
+    text = f'My name is {' '.join(names)} and I am {age} years old.'
+    if shout:
+        text = text.upper()
+    if file:
+        print(f"Saving to file {file.name}.")
+        file.write(text)
+    print(text)
+
+
+if __name__ == "__main__":
+    profile()
