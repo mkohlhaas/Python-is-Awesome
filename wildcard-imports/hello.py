f63c12247e3d96991938a4be13a16569812d281b
Hans-J. Schmid <knock@myopendoor.de>
2025-02-06 21:13:10 +0100

    wildcard imports

---
+++ wildcard-imports/hello.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# from math import cos, sin, pi
+# Don't use wildcard imports
+from math import *
+
+if __name__ == "__main__":
+    str = "My String"
+    print(str)
+
+    # Built-in function 'str' has been overridden
+    # y = 10
+    # z = str(y)
+
+    a = 42
+    b = sin(pi / 2)
+    print(b)
+    c = cos(pi / 2)
